cb838d126de581daa883a92bd1ccedd019e38e59
Erica <ericafrancine7@live.com>
2023-05-17 21:21:05 -0300

    Coletando a Speed

---
+++ speed.js
@@ -0,0 +1,32 @@
+const SPEED_ELEMENT = document.querySelector("#speed");
+const START_BTN = document.querySelector("#start");
+const STOP_BTN = document.querySelector("#stop");
+
+
+START_BTN.addEventListener("click", ()=>{
+    const OPTIONS = {enableHighAccuracy:true}
+
+    function handleSuccess(position){
+        SPEED_ELEMENT.innerText = position.coords.speed ? (position.coords.speed *3.6).toFixed(1) : 0
+    }
+    function handleError(error){
+        console.log(error.msg)
+    }
+
+    navigator.geolocation.watchPosition(handleSuccess, handleError, OPTIONS)
+    
+    
+    
+    START_BTN.classList.add("d-none")
+    STOP_BTN.classList.remove("d-none")
+
+});
+
+STOP_BTN.addEventListener("click", ()=>{
+    START_BTN.classList.remove("d-none")
+    STOP_BTN.classList.add("d-none")
+
+
+
+});
+
