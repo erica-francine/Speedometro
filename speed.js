f6d223d15378a4222f58ec7454ff6c44b0ff865a
Erica <ericafrancine7@live.com>
2023-05-24 21:23:29 -0300

    Armazenando dados no localstorage

--- speed.js
+++ speed.js
@@ -3,17 +3,20 @@ const START_BTN = document.querySelector("#start");
 const STOP_BTN = document.querySelector("#stop");
 
 let watchID = null;
+let currentRide = null;
 
 START_BTN.addEventListener("click", ()=>{
     const OPTIONS = {enableHighAccuracy:true}
 
     function handleSuccess(position){
+        addPosition(currentRide, position); //Adicionando a posição na minha currentRide
         SPEED_ELEMENT.innerText = position.coords.speed ? (position.coords.speed *3.6).toFixed(1) : 0
     }
     function handleError(error){
         console.log(error.msg)
     }
-
+    
+    currentRide = createNewRide() //Criando nova corrida quando ao apertar o start
     watchID = navigator.geolocation.watchPosition(handleSuccess, handleError, OPTIONS);
     
     
@@ -25,10 +28,14 @@ START_BTN.addEventListener("click", ()=>{
 
 
 STOP_BTN.addEventListener("click", ()=>{
+    if (!watchID)
+        return
     START_BTN.classList.remove("d-none")
     STOP_BTN.classList.add("d-none")
 
     navigator.geolocation.clearWatch(watchID);
     watchID = null;
+    updateStopTime(RIDE_ID);//Atualizando tempo de parada
+    currentRide = null;
 });
 
---
+++ storage.js
@@ -0,0 +1,46 @@
+
+//Função que cria a nova corrida, usando o Date.now como ID da corrida, e um objeto onde serão armazenados os dados dessa corrida
+function createNewRide(){
+    const RIDE_ID = Date.now();
+    const RIDE_RECORD = {
+        data:[],
+        startTime: RIDE_ID,
+        stopTime: null,
+    }
+
+    //Armazenando esses dados no localStorage
+    saveRideRecord(RIDE_ID, RIDE_RECORD);
+    return RIDE_ID
+}
+
+//Adicionando a posição na minha const RIDE_RECORD
+function addPosition(RIDE_ID, position){
+    const RIDE_RECORD = getRideRecord(RIDE_ID, position)
+    const NEW_DATA = {
+        accuracy: position.coords.accuracy,
+        altitude:position.coords.altitude,
+        altitudeAccuracy:position.coords.altitudeAccuracy,
+        heading:position.coords.heading,
+        latitude:position.coords.latitude,
+        longitude:position.coords.longitude,
+        speed:position.coords.speed,
+        timestamp: position.timestamp,
+    }
+    RIDE_RECORD.data.push(NEW_DATA)
+    saveRideRecord(RIDE_ID, RIDE_RECORD)//Adicionando dados atualizados no localStorage
+}
+
+function saveRideRecord(RIDE_ID, RIDE_RECORD){
+    localStorage.setItem(RIDE_ID, JSON.stringify(RIDE_RECORD))//Função para atualizar dados no localStorage
+}
+
+//Pegando o valor no meu localStorage para poder adicionar na posição
+function getRideRecord(RIDE_ID){
+    return JSON.parse(localStorage.getItem(RIDE_ID));
+}
+
+function updateStopTime(RIDE_ID){
+    const RIDE_RECORD = getRideRecord(RIDE_ID);
+    RIDE_RECORD.stopTime = Date.now();
+    saveRideRecord(RIDE_ID, RIDE_RECORD)
+}
