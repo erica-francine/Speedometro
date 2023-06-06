6b51ddd4b1519fc0df5221fc78e2e67f3d1e7e52
Erica <ericafrancine7@live.com>
2023-06-06 16:41:40 -0300

    Pegando distância e duração da corrida

--- index.js
+++ index.js
@@ -26,13 +26,24 @@ ALL_RIDES.forEach(async ([id, value]) => {
     MAX_SPEED.innerText = getMaxSpeed(RIDE.data)
 
 
+    const DISTANCE_DIV = document.createElement("div")
+    DISTANCE_DIV.innerText = getDistance(RIDE.data)
+
+    const DURATION_DIV = document.createElement("div")
+    DURATION_DIV.innerText = getDuration(RIDE);
 
     //Inserindo minha div da localidade dentro da li
     ITEM_ELEMENT.appendChild(CITY_DIV)
     //Inserindo minha div da velocidade máxima dentro da li
     ITEM_ELEMENT.appendChild(MAX_SPEED)
+    //Inserindo minha div do speed dentro da li
+    ITEM_ELEMENT.appendChild(DISTANCE_DIV)
+    //Inserindo minha div da duração da corrida dentro da li
+    ITEM_ELEMENT.appendChild(DURATION_DIV)
     //Inserindo minha li na ul 'rides'
     RIDE_ELEMENT.appendChild(ITEM_ELEMENT)
+
+    console.log(RIDE)
 
 
 })
@@ -61,27 +72,30 @@ function getDistance(positions) {
     const EARTH_RADIUS = 6371
     let totalDistance = 0;
 
-    for (let i = 0; i < positions.length; i++) {
+    for (let i = 0; i < positions.length - 1; i++) {
         const P1 = {
             latitude: positions[i].latitude,
             longitude: positions[i].longitude
         }
-
+        
         const P2 = {
             latitude: positions[i + 1].latitude,
             longitude: positions[i + 1].longitude
         }
 
-
         const DELTA_LATITUDE = toRad(P2.latitude - P1.latitude)
         const DELTA_LONGITUDE = toRad(P2.longitude - P1.longitude)
 
-        const a = Math.sin(DELTA_LATITUDE/2) * Math.sin(DELTA_LATITUDE/2) + 
-                  Math.sin(DELTA_LONGITUDE/2)* Math.sin(DELTA_LONGITUDE/2)
+        const A = Math.sin(DELTA_LATITUDE/2) * Math.sin(DELTA_LATITUDE/2) + 
+                  Math.sin(DELTA_LONGITUDE/2)* Math.sin(DELTA_LONGITUDE/2) * 
+                  Math.cos(toRad(P1.latitude)) * Math.cos(toRad(P2.latitude))
+
+        const C = 2 * Math.atan2(Math.sqrt(A), Math.sqrt(1-A)) 
 
 
+        const DISTANCE = EARTH_RADIUS * C
 
-
+        totalDistance += DISTANCE
     }
 
 
@@ -89,5 +103,23 @@ function getDistance(positions) {
         return degree * Math.PI / 180
     }
 
+    return totalDistance.toFixed(2)
+
 }
 
+function getDuration(ride){
+    const START_TIME = ride.startTime
+    const STOP_TIME = ride.stopTime
+    const DURATION = (STOP_TIME - START_TIME)
+
+    console.log(DURATION)
+    let seconds = Math.floor(DURATION/1000)
+    let hours = Math.floor(seconds/3600)
+    seconds %= 3600
+    let minutes = Math.floor(seconds/60)
+    seconds %= 60
+    console.log(seconds, hours, minutes)
+
+    const DURATION_FORMATTED = `${hours.toString().padStart(2,'0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+    return DURATION_FORMATTED
+}
